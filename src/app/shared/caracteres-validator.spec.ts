1acac736a0596d7389d6f72f7cc224b9b512ec1d
philippe chevry <phil_chevry@hotmail.com>
2018-03-29 22:50:07 -0400

    création second validateur maison

--- src/app/shared/caracteres-validator.spec.ts
+++ src/app/shared/caracteres-validator.spec.ts
@@ -16,17 +16,42 @@ describe('sansEspaces Validator',() => {
         expect(result['sansEspaces']).toBe(false);
      });
 
-     it('chaime phrase avec des mots  ' ,() => {
+     it('chaime phrase avec des mots est valide ' ,() => {
         let control = { value: 'je suis beau' };
         let validator = nombreCaractereValidator.sansEspaces();
         let result= validator(control as AbstractControl);
         expect(result['sansEspaces']).toBe(true);
      });
 
-     it('chaime phrase avec 3 espaces,des mots et ensuite 3 espaces  ' ,() => {
+     it('chaime phrase avec 3 espaces,des mots et ensuite 3 espaces est valide ' ,() => {
         let control = { value: '   bravo   '};
         let validator = nombreCaractereValidator.sansEspaces();
         let result= validator(control as AbstractControl);
         expect(result['sansEspaces']).toBe(true);
+     });
+     it('expression avec 1 espace et 2 caractères est invalide  ' ,() => {
+        let control = { value: ' xx'};
+        let validator = nombreCaractereValidator.longueurMinimum(3);
+        let result= validator(control as AbstractControl);
+        expect(result['longueurMinimum']).toBe(false);
+     });
+     it('expression avec 2 espace et 1 caractères est invalide  ' ,() => {
+        let control = { value: '  x'};
+        let validator = nombreCaractereValidator.longueurMinimum(3);
+        let result= validator(control as AbstractControl);
+        expect(result['longueurMinimum']).toBe(false);
+     });
+     it('une phrase  avec 3 espace et 3 caractères est valide  ' ,() => {
+        let control = { value: '   xxx'};
+        let validator = nombreCaractereValidator.longueurMinimum(3);
+        let result= validator(control as AbstractControl);
+        expect(result['longueurMinimum']).toBe(true);
+     });
+
+     it('une phrase  avec 5 espace,5 caractères  et 5 espaces est valide   ' ,() => {
+        let control = { value: '     xxxxx     '};
+        let validator = nombreCaractereValidator.longueurMinimum(5);
+        let result= validator(control as AbstractControl);
+        expect(result['longueurMinimum']).toBe(true);
      });
 });
